5f727a292e04a9580f6b5594f8558c4b6d3945bb
Daniel Valasek <daniel.valasek@yahoo.com>
2023-03-10 19:33:24 +0000

    Entry/Index done

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,6 +5,11 @@ from . import util
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 
+def content(request, title):
+    return render(request, "encyclopedia/content.html", {
+        "retrieve": util.get_entry(title),
+        "name": title
+    })
