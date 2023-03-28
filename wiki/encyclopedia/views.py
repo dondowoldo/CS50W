29783f8a31362cd7f2f286ec3c30561c46cd50fe
Daniel Valasek <daniel.valasek@yahoo.com>
2023-03-28 21:21:13 +0100

    create function added / TO DO : title validation

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
+
+class NewEntryForm(forms.Form):
+     title = forms.CharField(label="Title")
+     content = forms.CharField(widget=forms.Textarea(attrs={"rows": "20", "cols": "100"}))
 
 
 def index(request):
@@ -27,4 +33,21 @@ def search(request):
                 "matches": matches
                 })
         else:
-             return redirect(f"/wiki/{searched}")
+             return HttpResponseRedirect(reverse("wiki:title", args=[searched]))
+        
+def create(request):
+     if request.method == "GET":
+        return render(request, "encyclopedia/create.html", {
+           "form": NewEntryForm()
+     })
+     else:
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            util.save_entry(title, content)
+            return HttpResponseRedirect(reverse("wiki:title", args=[title]))
+        else:
+             return render(request, "encyclopedia/create.html", {
+                  "form": form
+             })
