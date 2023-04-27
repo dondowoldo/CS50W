b5740ce6bf20a3ee134f704af7116011a289bcaa
dondowoldo <116947425+dondowoldo@users.noreply.github.com>
2023-04-27 18:08:40 +0200

    dunno

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -29,7 +29,7 @@ class Bid(models.Model):
     item = models.ForeignKey(Listing, null=True, on_delete=models.CASCADE, related_name="item")
     date_created= models.DateTimeField(auto_now_add=True, null=True)
     price = models.PositiveIntegerField(blank=True)
-
+    
     def __str__(self):
         return f"{self.bidder.username} : {self.item.name} : {self.price} : {self.date_created}"
 
